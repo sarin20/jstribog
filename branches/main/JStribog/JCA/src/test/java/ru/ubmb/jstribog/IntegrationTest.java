94e41010b94065f30c0f6f920c14bac7798f5606
sarin2.0@gmail.com <sarin2.0@gmail.com@5d3c2199-d354-c65d-6816-50de017bc43b>
2014-03-26 11:45:05 +0000

    Version with no inner buffer for big files

---
+++ branches/main/JStribog/JCA/src/test/java/ru/ubmb/jstribog/IntegrationTest.java
@@ -0,0 +1,46 @@
+/*
+    Copyright (C) 2014  sarin
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ru.ubmb.jstribog;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Security;
+import org.junit.Test;
+
+public class IntegrationTest {
+    
+    private final boolean shortVersion = false;
+
+    @Test
+    public void testInstallation() throws NoSuchAlgorithmException, NoSuchProviderException, UnsupportedEncodingException {
+        if (Security.getProvider("JStribog") != null) {
+            Security.addProvider(new StribogProvider());
+        }
+        MessageDigest md = MessageDigest.getInstance(shortVersion ? "Stribog256" : "Stribog512", "JStribog");
+        byte[] bytes = new byte[]{
+            0x32, 0x31, 0x30, 0x39, 0x38, 0x37, 0x36, 0x35, 0x34, 0x33, 0x32, 0x31, 0x30, 0x39, 0x38, 0x37
+            , 0x36, 0x35, 0x34, 0x33, 0x32, 0x31, 0x30, 0x39, 0x38, 0x37, 0x36, 0x35, 0x34, 0x33, 0x32, 0x31
+            , 0x30, 0x39, 0x38, 0x37, 0x36, 0x35, 0x34, 0x33, 0x32, 0x31, 0x30, 0x39, 0x38, 0x37, 0x36, 0x35
+            , 0x34, 0x33, 0x32, 0x31, 0x30, 0x39, 0x38, 0x37, 0x36, 0x35, 0x34, 0x33, 0x32, 0x31, 0x30
+        };
+        md.digest(bytes);        
+    }
+
+}
